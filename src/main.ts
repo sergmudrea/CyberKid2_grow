949655ac71249523303f195e90e9132db239c6e3
Serghei Mudrea <schkyola@gmail.com>
2026-05-23 12:18:04 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { Game } from 'phaser';
 
 class SimpleScene extends Phaser.Scene {
   create() {
+    // Белый текст
     this.add.text(400, 300, 'CYBERKID', {
       fontSize: '64px',
       color: '#ffffff',
     }).setOrigin(0.5);
 
-    this.add.rectangle(400, 400, 100, 100, 0xff0000);
+    // Зелёный квадрат в центре
+    this.add.rectangle(400, 400, 100, 100, 0x00ff00);
+
+    // Красный квадрат в левом верхнем углу
+    this.add.rectangle(100, 100, 50, 50, 0xff0000);
+
+    // Жёлтый круг для проверки
+    this.add.circle(700, 500, 40, 0xffcc00);
   }
 }
 
