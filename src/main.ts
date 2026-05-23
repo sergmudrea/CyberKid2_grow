270e726fc6bba1a413b91e6e65acd2a0a5436c9b
Serghei Mudrea <schkyola@gmail.com>
2026-05-23 12:21:04 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,7 @@
 import { Game } from 'phaser';
-
-class SimpleScene extends Phaser.Scene {
-  create() {
-    // Белый текст
-    this.add.text(400, 300, 'CYBERKID', {
-      fontSize: '64px',
-      color: '#ffffff',
-    }).setOrigin(0.5);
-
-    // Зелёный квадрат в центре
-    this.add.rectangle(400, 400, 100, 100, 0x00ff00);
-
-    // Красный квадрат в левом верхнем углу
-    this.add.rectangle(100, 100, 50, 50, 0xff0000);
-
-    // Жёлтый круг для проверки
-    this.add.circle(700, 500, 40, 0xffcc00);
-  }
-}
+import { Preload } from './scenes/Preload';
+import { MainMenu } from './scenes/MainMenu';
+import { GameScene } from './scenes/GameScene';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.CANVAS,
@@ -25,7 +9,7 @@ const config: Phaser.Types.Core.GameConfig = {
   height: 600,
   backgroundColor: '#0a0a2a',
   parent: 'game-container',
-  scene: SimpleScene,
+  scene: [Preload, MainMenu, GameScene],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
