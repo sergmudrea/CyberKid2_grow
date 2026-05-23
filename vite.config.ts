075210fce906ebc8090d576ed8ebe9c7a09182ce
Serghei Mudrea <schkyola@gmail.com>
2026-05-23 21:24:00 +0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'es2020',
+    sourcemap: true,
+  },
+});
