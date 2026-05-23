a4e8c46d6dbd13604a87f120b465ce6789f2d188
Serghei Mudrea <schkyola@gmail.com>
2026-05-23 21:33:56 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
+// src/main.ts (обновлённый)
 import { Game } from 'phaser';
 import { Preload } from './scenes/Preload';
 import { MainMenu } from './scenes/MainMenu';
+import { GameScene } from './scenes/GameScene';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.CANVAS,
   width: window.innerWidth,
   height: window.innerHeight,
   backgroundColor: '#0a0a2a',
-  scene: [Preload, MainMenu],
+  scene: [Preload, MainMenu, GameScene],
   scale: {
     mode: Phaser.Scale.RESIZE,
     autoCenter: Phaser.Scale.CENTER_BOTH,
