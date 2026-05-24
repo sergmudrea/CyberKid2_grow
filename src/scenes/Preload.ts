e5e2a30ae5167cf161bf6ac6d0a520845752a998
Serghei Mudrea <schkyola@gmail.com>
2026-05-23 20:53:34 -0700

    Update Preload.ts

--- src/scenes/Preload.ts
+++ src/scenes/Preload.ts
@@ -1,11 +1,15 @@
 import { Scene } from 'phaser';
+import { levelManager } from '../managers/LevelManager';
 
 export class Preload extends Scene {
   constructor() {
     super('Preload');
   }
 
-  create(): void {
+  async create(): Promise<void> {
+    console.log('Preload: initializing level manager...');
+    await levelManager.initialize();
+    console.log('Preload: levels loaded, starting MainMenu');
     this.scene.start('MainMenu');
   }
 }
